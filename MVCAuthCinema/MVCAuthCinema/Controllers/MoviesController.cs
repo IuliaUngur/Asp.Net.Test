10304dc1a84420f0eca702632b869435d18864c1
Iulia Ungur <iulia.ungur@office.pitechnologies.ro>
2018-07-16 10:38:08 +0300

    Initial commit

---
+++ MVCAuthCinema/MVCAuthCinema/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cinema.Repo.Data;
+using Cinema.Repo.Models;
+
+namespace Cinema.Repo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ MVCAuthCinema/MVCAuthCinema/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Repo.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [MaxLength(80), Required]
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
